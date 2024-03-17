65b7b9840bf7782ab4d94348812cd7bf7c271c6b
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-17 15:04:18 +0100

    Create Controller Layer for List Posts Feature

---
+++ src/main/java/com/dgmf/controller/PostController.java
@@ -0,0 +1,29 @@
+package com.dgmf.controller;
+
+import com.dgmf.dto.PostDto;
+import com.dgmf.entity.Post;
+import com.dgmf.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    // Handler Method for HTTP GET Request
+    // ==> Returns Model and Logical View Name
+    @GetMapping("/admin/posts")
+    public String post(Model model) {
+        // Retrieve List of PostDto
+        List<PostDto> postDtos = postService.findAllPosts();
+        // Storing "postDtos" into the Model Object
+        model.addAttribute("posts", postDtos);
+        // Returns the Logical Thymeleaf View Name
+        return "/admin/posts";
+    }
+}
