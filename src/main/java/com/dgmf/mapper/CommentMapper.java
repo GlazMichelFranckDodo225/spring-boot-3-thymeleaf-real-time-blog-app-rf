fdd871bbb0cdcb47997d2738db53ff1fa1c1eab5
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-06 17:21:43 +0200

    Create CommentMapper Class

---
+++ src/main/java/com/dgmf/mapper/CommentMapper.java
@@ -0,0 +1,21 @@
+package com.dgmf.mapper;
+
+import com.dgmf.dto.CommentDto;
+import com.dgmf.entity.Comment;
+import lombok.*;
+import org.modelmapper.ModelMapper;
+
+@RequiredArgsConstructor
+public class CommentMapper {
+    private final ModelMapper modelMapper;
+
+    // Convert Comment Entity to Comment DTO
+    public CommentDto mapToCommentDto(Comment comment) {
+        return modelMapper.map(comment, CommentDto.class);
+    }
+
+    // Convert Comment DTO to Comment Entity
+    public Comment mapToCommentEntity(CommentDto commentDto) {
+        return modelMapper.map(commentDto, Comment.class);
+    } 
+}
